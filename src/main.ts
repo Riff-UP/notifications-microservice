fc4cc618303d2706889502fad224f1ff8e66f9bf
JuanMCamacho <243704@ids.upchiapas.edu.mx>
2026-02-14 19:00:29 -0600

    Docker Local Network TCP

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,19 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { envs } from './config';
 
 async function bootstrap() {
-
-  const logger = new Logger('Notifications-MS')
+  const logger = new Logger('Notifications-MS');
 
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.TCP,
-      options:{
-        port: envs.port
-      }
-    }
+      options: {
+        //Esta implementación permite el uso de la red de los containers de docker
+        //Permite el asilamiento del exterior priorizando seguridad
+        host: '0.0.0.0',
+        port: envs.port,
+      },
+    },
   );
   await app.listen();
   logger.log(`Microservice is listening on port ${envs.port}`);
